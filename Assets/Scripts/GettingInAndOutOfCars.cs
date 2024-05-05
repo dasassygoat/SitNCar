660957918e31cb1ffed3f71a81fa6a73bacabf68
Bryan Pritchett <bryan@coderbox.co>
2024-05-04 19:11:43 -0500

    fixed getting in and out

--- Assets/Scripts/GettingInAndOutOfCars.cs
+++ Assets/Scripts/GettingInAndOutOfCars.cs
@@ -1,23 +1,20 @@
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityStandardAssets.Cameras;
 using UnityStandardAssets.Vehicles.Car;
 
 public class GettingInAndOutOfCars : MonoBehaviour
 {
-    [Header("Human")]
-    [SerializeField] private GameObject human = null;
-    
+    [FormerlySerializedAs("camera")] [Header("Camera")] [SerializeField] AutoCam mCamera = null;
+
+    [Header("Human")] [SerializeField] private GameObject human = null;
+
     [Space, Header("Car Stuff")] [SerializeField]
     private GameObject car = null;
 
     [SerializeField] private CarUserControl carController = null;
-    
-    [Header("Input")]
-    [SerializeField] private KeyCode enterExitKey = KeyCode.E;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    [Header("Input")] [SerializeField] private KeyCode enterExitKey = KeyCode.E;
 
     // Update is called once per frame
     void Update()
@@ -25,14 +22,20 @@ public class GettingInAndOutOfCars : MonoBehaviour
         if (Input.GetKeyDown(enterExitKey))
         {
             human.SetActive(!human.activeSelf);
-            if(!human.activeSelf )GetOutOfCar();
+            if (human.activeSelf) GetOutOfCar();
+            else GetIntoCar();
         }
+    }
+
+    private void GetIntoCar()
+    {
+        carController.enabled = true;
     }
 
     void GetOutOfCar()
     {
-        //human.SetActive(true);
         human.transform.position = car.transform.position + car.transform.TransformDirection(Vector3.left) * 2;
+        mCamera.SetTarget(human.transform);
         carController.enabled = false;
     }
 }
